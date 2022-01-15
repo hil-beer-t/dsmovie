34f7b072e37834365ffe6762c4463ffc0151f431
hil-beer-t <hil.beer.t.git@gmail.com>
2022-01-15 04:02:49 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dsmovie.entities;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_movie")
@@ -14,6 +16,9 @@ public class Movie {
     private Double score;
     private Integer count;
     private String Image;
+
+    @OneToMany(mappedBy = "id.movie")
+    private Set<Score> scores = new HashSet<>();
 
     public Movie() {
     }
@@ -64,5 +69,9 @@ public class Movie {
 
     public void setImage(String image) {
         Image = image;
+    }
+
+    public Set<Score> getScores() {
+        return scores;
     }
 }
